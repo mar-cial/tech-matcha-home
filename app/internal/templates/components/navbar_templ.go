ce58011e20ed89817f2070fd75baadff5fbdc47a
Cesar <cesar.marcial@cabrasportshq.com>
2024-12-04 13:01:23 -0800

    Add links that push to browser history to navbar

--- app/internal/templates/components/navbar_templ.go
+++ app/internal/templates/components/navbar_templ.go
@@ -144,7 +144,7 @@ func Navbar() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<a href=\"\" hx-target=\"body\" hx-get=\"/register\" class=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<a href=\"\" hx-push-url=\"true\" hx-target=\"body\" hx-get=\"/register\" class=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
